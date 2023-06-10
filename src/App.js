930e3f292eb4b2fd1217027ed2eeff0461c72698
ojeaga trust <trustirooj@gmail.com>
2023-06-10 13:54:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello beautiful {value}</div>;
+  return <div>Hello beautiful wddddwedwedwdwqdwdf {value}</div>;
 }
 
 export default App;
